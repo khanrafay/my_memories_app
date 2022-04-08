a69ac7259619c8374d9e889a46a7c84e7030dc24
Abdul Rafay Khan <rafaykhan.work@gmail.com>
2022-04-08 15:16:45 +0300

    backend

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { createPost, getPosts } from '../controller/posts.js';
+import { createPost, getPosts, updatePost } from '../controller/posts.js';
 
 const router = express.Router();
 
 
 router.get('/', getPosts)
 router.post('/', createPost)
+router.patch('/:id', updatePost)
 
 export default router;
